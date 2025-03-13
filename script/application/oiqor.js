836d5dc0de8d31f7c546eed8ee09745937235a8b
Kuanysh Rakhmetolla <kuan0555kz@gmail.com>
2025-03-14 01:47:13 +0500

    Update oiqor.js

--- script/application/oiqor.js
+++ script/application/oiqor.js
@@ -7,7 +7,7 @@ const deleteChatButton = document.querySelector("#delete-chat-button");
 let userMessage = null;
 let isResponseGenerating = false;
 
-const API_KEY = "AIzaSyBbYhWT3mPrlokAIUTgraXwEeT9bAY2Aus";
+const API_KEY = "";
 const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
 
 const loadDataFromLocalstorage = () => {
